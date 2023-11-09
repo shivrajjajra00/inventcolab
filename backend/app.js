b62d434334d8603496f9153db4b3c98ad813c375
vgokaldas <vinay@pasiv.io>
2023-11-09 14:15:40 +0530

    update

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,23 @@
 const connection = require("./connection")
-
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const adminRoutes = require("./src/routers/adminrouter")
 
 const express = require("express");
 const app = express();
-app.use(express.json());
 
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true, // Enable access-control-allow-credentials
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
 app.use("/admin", adminRoutes);
-app.use("/resources", resourcesRouter);
+app.use(bodyParser.json());
+
+// app.use("/resources", resourcesRouter);
 
 app.listen(4040, () => {
   console.log("server run");
