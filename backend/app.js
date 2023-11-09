9a41045d717ad459e8fdb3aeed6f10354bbbf73d
vgokaldas <vinay@pasiv.io>
2023-11-09 10:43:26 +0530

    stage

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,12 @@
 const connection = require("./connection")
-
-
 const express = require("express");
-
 const app = express();
 
+const clientrouter = require("./src/routers/clientrouter.js");
 
+app.use(express.json()); // Add this line to enable JSON request body parsing
+app.use("/client", clientrouter);
 
-app.listen(5000, () => {
+app.listen(4040, () => {
   console.log("server run");
 });
