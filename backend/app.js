7cb258256369271b9456abd48b25fb490f6ac58f
shivrajjajra <shivrajjajra00@gmail.com>
2023-11-14 14:25:18 +0530

    update backend

--- backend/app.js
+++ backend/app.js
@@ -6,19 +6,15 @@ const resourcesRouter = require("./src/routers/resourcesRouter");
 const express = require("express");
 const app = express();
 
-const corsOptions = {
-  origin: "http://localhost:3000",
-  credentials: true,
-  optionSuccessStatus: 200,
-};
 
-app.use(cors(corsOptions));
+
+app.use(cors());
 app.use(express.json());
 app.use("/admin", adminRoutes);
-console.log("connect");
+
 
 app.use("/resources", resourcesRouter);
 
-app.listen(4040, () => {
+app.listen(4041, () => {
   console.log("server run");
 });
--- backend/src/routers/clientrouter.js
+++ backend/src/routers/clientrouter.js
@@ -1,38 +1,15 @@
 const express = require("express")
 const router = express.Router()
 const clientcontroller = require('../controllers/clientcontroller.js');
-const clientlistcontroller = require("../controllers/clientcontroller.js")
+
 // const { validateDbId, verifyToken } = require('../middlewares');
 
 router.post('/users', async (req, res) => {
     clientcontroller.createUser(req, res);
 });
-router.post("/addproduct", clientlistcontroller.tokenVerification);
-router.get("/productlist", clientlistcontroller.getProductList);
-router.get("/findone/:id", clientlistcontroller.findOneProduct);
-router.put("/update/:id", clientlistcontroller.updateProductList);
-router.delete("/delete/:id", clientlistcontroller.deleteProduct);
-router.get("/search/:key", clientlistcontroller.searchProduct);
-
-// router.get('/:id', async (req, res) => {
-//     try {
-//         await dashboardcontroller.getById(req, res, next);
-//     } catch (err) {
-//         // Handle any errors here
-//         console.error(err);
-//         res.status(500).json({
-//             status: 500,
-//             message: "Internal server error",
-//             data: ["Error in getById:", err.message],
-//         });
-//     }
-// })
 
 router.get('/users', clientcontroller.getAllUsers);
-
-// Assuming you have a router set up
 router.delete('/users/:id', clientcontroller.deleteUser);
-
 router.get('/users/:id', clientcontroller.getUserById);
 
 module.exports = router;
